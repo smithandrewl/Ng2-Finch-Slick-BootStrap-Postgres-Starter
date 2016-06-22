143d010209816710ff9876f050a123fdd982805f
Andrew Smith <smith.andrew.l@gmail.com>
2016-06-22 13:13:37 +0000

    Full round-trip to db from REST

--- backend/src/main/scala/main.scala
+++ backend/src/main/scala/main.scala
@@ -1,16 +1,36 @@
 import com.twitter.finagle.Http
 import com.twitter.server.TwitterServer
-import com.twitter.util.Await
+import com.twitter.util.{Await, Duration}
 import io.finch._
 import slick.driver.PostgresDriver.backend._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util._
+
+
 object Main extends TwitterServer {
   def main() {
-    val db: Database = Database.forURL("jdbc:postgresql://localhost/many_tasks", user = "many_tasks_user", driver = "org.postgresql.Driver")
 
-    val sess = db.createSession()
+    val api: Endpoint[String] = get("users") {
 
-    val api: Endpoint[String] = get("hello") { Ok("Hello World!")}
+      val users = tables.AuthDAO.getUsers()
+
+      // users.onSuccess(usr => s"id = $usr.id name = $usr.name hash = $usr.hash isAdmin= $usr.isAdmin")
+      @volatile
+      var msg: String = ""
+
+      users.onComplete {
+        case Success(usrs) => msg = "{\"id\":" + usrs(0)._1 + ", \"name\": \"" + usrs(0)._2 + "\", \"hash\": \"" + usrs(0)._3 + "\",  \"isAdmin\": \"" + usrs(0)._4 + "\"}"
+        case Failure(e)    => e.printStackTrace()
+      }
+
+      while(!users.isCompleted) {
+        Thread.sleep(10)
+      }
+
+      Ok(msg)
+
+    }
 
 
     val server = Http.serve(":8080", api.toService)
@@ -19,6 +39,6 @@ object Main extends TwitterServer {
       server.close()
     }
 
-    Await.ready(server)
+     com.twitter.util.Await.ready(server)
   }
 }
--- backend/src/main/scala/tables.scala
+++ backend/src/main/scala/tables.scala
@@ -1,1 +1,33 @@
-//import slick.profile.RelationalTableComponent.Table
+import com.twitter.finagle.zipkin.thriftscala.Scribe.Log
+import slick.ast.ColumnOption.PrimaryKey
+import slick.lifted.{TableQuery, Tag}
+import slick.driver.PostgresDriver.api._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+object tables {
+  val db: Database = Database.forURL("jdbc:postgresql://localhost/many_tasks", user = "many_tasks_user", driver = "org.postgresql.Driver")
+
+
+
+  class Auth(tag: Tag) extends Table[(Int, String, String, Boolean)](tag, "auth") {
+    def authId   = column[Int]("authid", PrimaryKey)
+    def username = column[String]("username")
+    def hash     = column[String]("hash")
+    def isAdmin  = column[Boolean]("isadmin")
+
+    def * = (authId, username, hash, isAdmin)
+  }
+
+  val users = TableQuery[Auth]
+
+  object AuthDAO {
+
+    def getUsers() (implicit e :ExecutionContext): Future[Seq[(Int, String, String, Boolean)]] = {
+
+      db.run(users.result)
+    }
+
+  }
+}
