b601e104a32c54ed2caf4c10aff78059d6375cb4
Andrew Smith <smith.andrew.l@gmail.com>
2016-06-30 11:41:31 -0500

    Added a component for the login screen

---
+++ frontend/src/app/login-screen/login-screen.component.ts
@@ -0,0 +1,20 @@
+import { ViewChild, Component, OnInit } from '@angular/core';
+import {LoginAppComponent} from '../login.component';
+@Component({
+  moduleId: module.id,
+  selector: 'app-login-screen',
+  templateUrl: 'login-screen.component.html',
+  styleUrls: ['login-screen.component.css'],
+  directives: [LoginAppComponent]
+})
+export class LoginScreenComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  @ViewChild('frontend-app')
+  login: LoginAppComponent;
+
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { LoginAppComponent, environment } from './app/';
+import { LoginScreenComponent} from './app/login-screen/';
 import {HTTP_PROVIDERS, Http} from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(LoginAppComponent, [HTTP_PROVIDERS]);
+bootstrap(LoginScreenComponent, [HTTP_PROVIDERS]);
 
