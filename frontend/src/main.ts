f81e7738712cc7d8181fe24f00f0ef261445f491
Andrew Smith <smith.andrew.l@gmail.com>
2016-07-07 10:14:29 -0500

    Removed admin-dashboard files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { LoginAppComponent, environment } from './app/';
 import { LoginScreenComponent} from './app/login-screen/';
-import { AdminDashboardComponent} from './app/admin-dashboard';
 import {HTTP_PROVIDERS, Http} from '@angular/http';
 
 if (environment.production) {
