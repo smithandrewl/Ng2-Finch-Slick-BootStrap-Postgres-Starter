c15df17c5f18b9185303fb7f940cdda920f697ae
Andrew Smith <smith.andrew.l@gmail.com>
2016-07-07 10:59:53 -0500

    Routes load

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { LoginAppComponent} from './';
+
+export const AppRoutes = [
+    { path: '', component: LoginAppComponent },
+];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,15 @@ import { enableProdMode } from '@angular/core';
 import { LoginAppComponent, environment } from './app/';
 import { LoginScreenComponent} from './app/login-screen/';
 import {HTTP_PROVIDERS, Http} from '@angular/http';
+import { provideRouter } from '@angular/router';
+import { AppRoutes } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(LoginScreenComponent, [HTTP_PROVIDERS]);
+bootstrap(LoginScreenComponent, [
+  provideRouter(AppRoutes),
+  HTTP_PROVIDERS
+]);
 
